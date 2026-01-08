2153127c8d1be3301adc42011433063b405590b2
LimRaynor <morden9137@gmail.com>
2026-01-08 18:15:37 +0900

    CQRS 을 이용한 보안권한

--- 10.LimWonSuk/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/command/application/controller/ProductCommandController.java
+++ 10.LimWonSuk/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/command/application/controller/ProductCommandController.java
@@ -1,17 +1,15 @@
 package com.ohgiraffers.cqrs.product.command.application.controller;
 
-import com.ohgiraffers.cqrs.product.command.application.dto.response.ProductCommandResponse;
 import com.ohgiraffers.cqrs.common.dto.ApiResponse;
 import com.ohgiraffers.cqrs.product.command.application.dto.request.ProductCreateRequest;
-
+import com.ohgiraffers.cqrs.product.command.application.dto.request.ProductUpdateRequest;
+import com.ohgiraffers.cqrs.product.command.application.dto.response.ProductCommandResponse;
 import com.ohgiraffers.cqrs.product.command.application.service.ProductCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /* 데이터 변경 명령 담당 (Command Side) */
@@ -28,7 +26,7 @@ public class ProductCommandController {
     public ResponseEntity<ApiResponse<ProductCommandResponse>> createProduct(
             @RequestPart @Validated ProductCreateRequest productCreateRequest,
             @RequestPart MultipartFile productImg
-            ){
+    ){
 
         /* 전달 받은 ProductCreateRequest의 데이터를 이용해
          * DB에 새 데이터 삽입 후 삽입된 행의 PK(productCode) 반환 받기
@@ -46,5 +44,32 @@ public class ProductCommandController {
                 .body(ApiResponse.success(response));
     }
 
+    /* 상품 수정 */
+    @PutMapping("/products/{productCode}")
+    public ResponseEntity<ApiResponse<Void>> updateProduct(
+            @PathVariable("productCode") Long productCode,
+            @RequestPart @Validated ProductUpdateRequest productUpdateRequest,
+            @RequestPart(required = false) MultipartFile productImg
+    ){
+
+        productCommandService.updateProduct(
+                productCode,
+                productUpdateRequest,
+                productImg
+        );
+
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+/* 상품 삭제 */
+    @DeleteMapping("/products/{productCode}")
+    public ResponseEntity<ApiResponse<Void>> deleteProduct(
+            @PathVariable Long productCode
+    ){
+        productCommandService.deleteProduct(productCode);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body(ApiResponse.success(null));
+        }
 
 }
--- 10.LimWonSuk/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/command/domain/repository/ProductRepository.java
+++ 10.LimWonSuk/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/command/domain/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package com.ohgiraffers.cqrs.product.command.domain.repository;
 
 import com.ohgiraffers.cqrs.product.command.domain.aggregate.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository {
     Product save(Product newProduct);
+
+    Optional<Product> findById(Long productCode);
+
+    void deleteById(Long productCode);
 }
